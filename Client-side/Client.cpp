7dbe5f18d1ba96442607c94f881b5eed55333cde
Rák Szabolcs <rakszabi@gmail.com>
2020-01-17 13:25:59 +0100

    Add files via upload

--- Client-side/Client.cpp
+++ Client-side/Client.cpp
@@ -21,7 +21,7 @@ void Client::buildConnect() {
     serv_addr.sin_port = htons(PORT);
 
     //Connect IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+    if (inet_pton(AF_INET, "192.168.255.117", &serv_addr.sin_addr)<=0) {
         cout << "Invalid address or address not supported" << endl;
     }
 
